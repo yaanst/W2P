5475b4468a765ceb35f80dc8eb4f3a6d3969f9e4
nbonfils <nils.bonfils@epfl.ch>
2018-01-08 15:10:48 +0100

    Add constructors for async data structures + new String() method for Peer object

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,7 @@
 package structs
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -14,14 +15,14 @@ type Peer net.UDPAddr
 
 // Peers is a collection of Peer
 type Peers struct {
-	mux sync.Mutex
-	p   []*Peer
+	Mux sync.Mutex
+	P   []*Peer
 }
 
 // WebsiteMap is a map from a Website PubKey to the Website
 type WebsiteMap struct {
-	mux sync.Mutex
-	w   map[string]*Website
+	Mux sync.Mutex
+	W   map[string]*Website
 }
 
 // Website is a structure that represents a website
@@ -35,6 +36,38 @@ type Website struct {
 	Version     int
 }
 
+// RoutingTable is a table which keeps in memory possible route for a dest
+// if a Peer is not directly reachable
+type RoutingTable struct {
+	Mux sync.Mutex
+	R   map[string]*Peers
+}
+
+// ----------------
+// - Constructors -
+// ----------------
+
+// NewPeers constructs a new Peers object (list of peer with a mutex)
+func NewPeers() *Peers {
+	return &Peers{
+		P: make([]*Peer, 5),
+	}
+}
+
+// NewWebsiteMap constructs a WebsiteMap object
+func NewWebsiteMap() *WebsiteMap {
+	return &WebsiteMap{
+		W: make(map[string]*Website),
+	}
+}
+
+// NewRoutingTable constructs a RoutingTable object
+func NewRoutingTable() *RoutingTable {
+	return &RoutingTable{
+		R: make(map[string]*Peers),
+	}
+}
+
 // -----------
 // - Methods -
 // -----------
@@ -43,60 +76,57 @@ type Website struct {
 
 // PeerEquals compare two peers
 func PeerEquals(p1, p2 *Peer) bool {
-	return p1.IP.String() == p2.IP.String() && p1.Port == p2.Port
+	return p1.String() == p2.String()
+}
+
+func (p *Peer) String() string {
+	return fmt.Sprintf("%v:%v", p.IP.String(), p.Port)
 }
 
 // Peers
 
-// NewPeers create a new Peers object (list of peer with a mutex)
-func NewPeers() *Peers {
-	return &Peers{
-		p: make([]*Peer, 5),
-	}
-}
-
 // Contains check if the Peers slice contains a Peer
 func (peers *Peers) Contains(peer *Peer) bool {
-	peers.mux.Lock()
-	for _, p := range peers.p {
+	peers.Mux.Lock()
+	for _, p := range peers.P {
 		if PeerEquals(p, peer) {
-			peers.mux.Unlock()
+			peers.Mux.Unlock()
 			return true
 		}
 	}
-	peers.mux.Unlock()
+	peers.Mux.Unlock()
 	return false
 }
 
 // Add adds a Peer to the Peers if not already present
 func (peers *Peers) Add(peer *Peer) {
 	if !peers.Contains(peer) {
-		peers.mux.Lock()
-		peers.p = append(peers.p, peer)
-		peers.mux.Unlock()
+		peers.Mux.Lock()
+		peers.P = append(peers.P, peer)
+		peers.Mux.Unlock()
 	}
 }
 
 // Remove removes a Peer from the Peers
 func (peers *Peers) Remove(peer *Peer) {
-	peers.mux.Lock()
-	for i, p := range peers.p {
+	peers.Mux.Lock()
+	for i, p := range peers.P {
 		if PeerEquals(p, peer) {
 			// cut slice in 2 part and append without i-th element inbetween
-			peers.p = append(peers.p[:i], peers.p[i+1:]...)
+			peers.P = append(peers.P[:i], peers.P[i+1:]...)
 			break
 		}
 	}
-	peers.mux.Unlock()
+	peers.Mux.Unlock()
 }
 
 // WebsiteMap
 
 // Set adds/updates a website to the website map
 func (wm *WebsiteMap) Set(website *Website) {
-	wm.mux.Lock()
-	wm.w[website.Name] = website
-	wm.mux.Unlock()
+	wm.Mux.Lock()
+	wm.W[website.Name] = website
+	wm.Mux.Unlock()
 }
 
 // Website

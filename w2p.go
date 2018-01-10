1928440917b49d076e117bc2adc0ff73d4dfd1bc
nbonfils <nils.bonfils@epfl.ch>
2018-01-10 07:27:25 +0100

    Fix command line argument parsing

--- w2p.go
+++ w2p.go
@@ -12,13 +12,14 @@ func main() {
 	//clientPort := flag.String("client-port", "4000", "Port on which you connect the browser")
 	//publicAddr := flag.String("public-addr", "", "Address used to communicate with other peers in the form of IP:PORT")
 	var name, addr, peers string
-	flag.StringVar(&peers, "peers", "", "Comma-separated list of peers in the form of IP:PORT")
+	flag.StringVar(&name, "name", "test", "Name of the node")
 	flag.StringVar(&addr, "addr", "", "Address of the node format IP:PORT")
-	flag.StringVar(&name, "name", "", "Name of the node")
+	flag.StringVar(&peers, "peers", "", "Comma-separated list of peers in the form of IP:PORT")
+	flag.Parse()
 
-	log.Println("arg name: " + name)
-	log.Println("arg addr: " + addr)
-	log.Println("arg peers: " + peers)
+	log.Println("arg name:", name)
+	log.Println("arg addr:", addr)
+	log.Println("arg peers:", peers)
 
 	node := node.NewNode(name, addr, peers)
 	node.Init()
